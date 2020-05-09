10805ba4ba7f3f841748591ad1f89eb5426fdc51
Pablo Reynoso <p.reynoso.mena@gmail.com>
2020-05-08 22:07:42 -0500

    Project CRUD

---
+++ src/app.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAQA5sZXNXEblC1X8S13NdBi64sTyNaiTY",
+  authDomain: "angularcrud-82c41.firebaseapp.com",
+  databaseURL: "https://angularcrud-82c41.firebaseio.com",
+  projectId: "angularcrud-82c41",
+  storageBucket: "angularcrud-82c41.appspot.com",
+  messagingSenderId: "366187388172",
+  appId: "1:366187388172:web:1c6c5437d390e23c913d29",
+  measurementId: "G-3T4QTW0NJE"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,74 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angularproject';
+  title: string = 'Angular CRUD';
+
+  msg: string = ""
+
+  employees = [
+    { 'name': 'Pablo Reynoso', position: 'Ingeniero', email: 'p.reynoso.mena@gmail.com' },
+    { 'name': 'Juan Rulfo', position: 'Escritor', email: 'juanrulfo@gmail.com' },
+    { 'name': 'Ressendi', position: 'Pintor', email: 'ressendi@gmail.com' }
+  ]
+
+  model: any = {
+
+  }
+
+  model2: any = {
+
+  }
+
+  closeAlert(): void {
+    this.msg = ""
+  }
+
+  addEmployee(): void {
+    console.log("AGREGANDO")
+    this.employees.push(this.model)
+    this.msg = "Added Field"
+    setTimeout(
+      () => {
+        this.msg = ""
+      }
+      , 3000)
+  }
+
+  deleteEmployee(i): void {
+    var answer = confirm('Are you want delete?')
+    if (answer) {
+      this.employees.splice(i, 1)
+      this.msg = "Deleted Field"
+      setTimeout(
+        () => {
+          this.msg = ""
+        }
+        , 3000)
+    }
+  }
+
+  myValue;
+  editEmployee(i): void {
+    this.model2.name = this.employees[i].name
+    this.model2.position = this.employees[i].position
+    this.model2.email = this.employees[i].email
+    this.myValue = i;
+
+  }
+
+  updateEmployee(): void {
+    let i = this.myValue;
+    for (let j = 0; j < this.employees.length; j++) {
+      if (i == j) {
+        this.employees[i] = this.model2
+        this.model2 = {}
+        this.msg = "Updated Field"
+        setTimeout(
+          () => {
+            this.msg = ""
+          }
+          , 3000)
+      }
+    }
+  }
 }
